b519e7afa616e975b5fbb8a1bbcf4db042d79b60
Pat <stumbaup@oregonstate.edu>
2021-03-11 16:05:16 -0800

    button align

--- wasteagram/lib/widgets/form_widget.dart
+++ wasteagram/lib/widgets/form_widget.dart
@@ -46,50 +46,47 @@ class _FormWidgetState extends State<FormWidget> {
               },
             ),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: SizedBox(
-                width: 2000,
-                height: 75,
-                child: Semantics(
-                    label: "Submit",
-                    hint: "Submit",
-                    child: RaisedButton(
-                      key: Key('submitButton'),
-                      onPressed: () async {
-                        if (formKey.currentState.validate()) {
-                          formKey.currentState.save();
-                          // Format Date
-                          String date = DateFormat.yMd().format(DateTime.now());
+          SizedBox(
+              width: 2000,
+              height: 75,
+              child: Semantics(
+                  label: "Submit",
+                  hint: "Submit",
+                  child: RaisedButton(
+                    key: Key('submitButton'),
+                    onPressed: () async {
+                      if (formKey.currentState.validate()) {
+                        formKey.currentState.save();
+                        // Format Date
+                        String date = DateFormat.yMd().format(DateTime.now());
 
-                          // Get location data
-                          await retrieveLocation();
-                          StorageReference storageReference = FirebaseStorage
-                              .instance
-                              .ref()
-                              .child(DateTime.now().toString());
+                        // Get location data
+                        await retrieveLocation();
+                        StorageReference storageReference = FirebaseStorage
+                            .instance
+                            .ref()
+                            .child(DateTime.now().toString());
 
-                          //StorageUploadTask uploadTask = storageReference.putFile(File(imagePath));
+                        //StorageUploadTask uploadTask = storageReference.putFile(File(imagePath));
 
-                          //await uploadTask.onComplete;
-                          final url = imagePath;
+                        //await uploadTask.onComplete;
+                        final url = imagePath;
 
-                          await Firestore.instance.collection('posts').add({
-                            'date': date,
-                            'itemCount': itemCount,
-                            'latitude': locationData.latitude.toString(),
-                            'longitude': locationData.longitude.toString(),
-                            'url': url
-                          });
-                          Navigator.pop(context);
-                        }
-                      },
-                      child: Icon(
-                        Icons.add_to_photos,
-                        size: 40,
-                      ),
-                    ))),
-          ),
+                        await Firestore.instance.collection('posts').add({
+                          'date': date,
+                          'itemCount': itemCount,
+                          'latitude': locationData.latitude.toString(),
+                          'longitude': locationData.longitude.toString(),
+                          'url': url
+                        });
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: Icon(
+                      Icons.cloud_upload_outlined,
+                      size: 50,
+                    ),
+                  ))),
         ]));
   }
 
