9f2b508855b1ee9fe097dffc19d380b989140c66
Pat <stumbaup@oregonstate.edu>
2021-03-13 08:59:33 -0800

    added orientations

--- wasteagram/lib/main.dart
+++ wasteagram/lib/main.dart
@@ -1,5 +1,10 @@
 import 'imports.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.portraitUp
+  ]);
   runApp(MyApp());
 }
